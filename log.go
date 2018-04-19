6a425f957f6c4cbb552a2b82e419b4a4189c0ad4
PORNX <33781000+PORNX@users.noreply.github.com>
2018-04-19 12:50:52 +0300

    Update log.go

--- log.go
+++ log.go
@@ -10,6 +10,7 @@ import (
 
 // log is a logger that is initialized with no output filters.  This
 // means the package will not perform any logging by default until the caller
+// means the package will not perform any logging by default until the caller
 // requests it.
 var log btclog.Logger
 
